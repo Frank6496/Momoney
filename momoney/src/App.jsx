e7fc8b89d0ad0319e27df3361b2180b200d8aa84
Frank6496 <francismuthoni254@gmail.com>
2022-11-09 13:02:30 +0300

    components created

--- momoney/src/App.jsx
+++ momoney/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { useContext } from "react";
+import { BrowserRouter ,Routes ,Route, Link } from "react-router-dom";
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Context } from "./context/Context";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {user} = useContext(Context);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+    <div className='flex flex-col justify-between h-screen'>
+      <Header/>
+      <Routes>        
+         <Route path="/" element={<Home />} />
+         <Route path="register" element={<Register />} />
+         <Route path="login" element={<Login />} />
+         <Route path="profile/:userId" element={user ? <Profile /> : <Home/>} /> /profile/Transfer/s=124214
+         <Route path="profile/update/:userId" element={user ? <ProfileEdit/> : <Home />} />
+         <Route path="*" 
+              element={
+                        <div className='shadow-lg grid place-self-center mt-20 py-20 w-3/4 mx-auto text-white  justify-center text-center'>
+                          <p className='text-7xl pt-3 m-3'>😮404😮</p>
+                          <p className='text-lg  m-2'>There's nothing here!</p>
+                          <Link className='btn btn-accent text-2xl' to="/">🏡Home</Link>
+                        </div>
+              }/>
+      </Routes>
+      <Footer/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
